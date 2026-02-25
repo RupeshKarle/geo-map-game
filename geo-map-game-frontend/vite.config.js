8e39d0dc3358873d45c19793411aa70605319545
RupeshKarle <rupeshkarle.gen@gmail.com>
2026-02-25 18:02:55 +0530

    frontend

--- geo-map-game-frontend/vite.config.js
+++ geo-map-game-frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/geo-map-game-frontend/",
+  base: "/geo-map-game/",
   plugins: [react()],
 })
